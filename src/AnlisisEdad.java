a95ece9b220ad2af917d1047caea75aea0fd4872
Pedrule5710 <m.r.pedrule@gmail.com>
2024-06-13 10:22:50 +0200

    Primer commit añadimos el proyecto con sus 2 clases

---
+++ src/AnlisisEdad.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class AnlisisEdad {
+	private ArrayList<Integer> edades = new ArrayList<Integer>();
+	public AnlisisEdad(ArrayList<Integer> edades) {
+	this.edades = edades;
+
+	}
+	
+	public String comprobar(ArrayList<Integer> edades) {
+		int suma=0, cuenta=0;
+		for(int i =0; i<edades.size();i++) {
+			if(edades.get(i)%2 == 0){
+				suma=suma + edades.get(i);
+			}else if(edades.get(i)%2 != 0) {
+				cuenta = cuenta + 1;
+			}
+		}
+		System.out.println("Suma de pares: " + suma + " ,recuento de impares: " + cuenta);
+		
+		
+		return ("Suma de pares: " + suma + " ,recuento de impares: " + cuenta);
+	}
+
+}
---
+++ src/PrinciplaApp.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class PrinciplaApp {
+	public static void main(String args[]) {
+		ArrayList<Integer> edades = new ArrayList<Integer>();
+		edades.add(15);
+		edades.add(44);
+		edades.add(58);
+		edades.add(68);
+		edades.add(32);
+		edades.add(74);
+		edades.add(14);
+		edades.add(147);
+		edades.add(78);
+		
+		AnlisisEdad prueba = new AnlisisEdad(edades);
+		prueba.comprobar(edades);
+
+	}
+
+}
